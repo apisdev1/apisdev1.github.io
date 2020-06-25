9630c4fa243f1d9f6d375df30eca7889aee23e06
Hal Rodriguez <halr66@gmail.com>
2020-06-25 15:59:26 -0700

    Added demo display toggle

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,26 @@
 /* index.js */
 
 async function reportAttempt(credential) {
-    console.log("reportAttempt: " + credential);
-    console.log("reportAttempt: currently not implemented!");
-    /* TODO: use the Fetch API */
+    //console.log(`reportAttempt: ${credential} (currently not implemented!)`);
+    //TODO: use the Fetch API
+    content = document.getElementById('glyph').innerHTML
+    if (content == '\u278A') {
+        setOccupancyDisplay(3);
+    } else {
+        setOccupancyDisplay(2);
+    }
+}
+
+function setOccupancyDisplay(occupancy) {
+    //console.log(`setOccupancyDisplay: ${occupancy}`);
+    if (occupancy == 2) {
+        document.getElementById('glyph').innerHTML = '\u278A';
+        document.getElementById('occupancy').innerHTML = '1 out of 3 spaces are available in this space.';
+        document.getElementById('instruction').innerHTML = 'Tap your fob to check in or out of this room.';
+    } else {
+        // Max Capacity
+        document.getElementById('glyph').innerHTML = '\u2716';
+        document.getElementById('occupancy').innerHTML = 'Space is now at capacity: 3 spots out of 3 are taken.';
+        document.getElementById('instruction').innerHTML = 'Please wait until a spot open up.';
+    }
 }
