f3133f5da8f0af1208d65df610aa2400546d5529
Hal Rodriguez <halr66@gmail.com>
2020-06-26 11:32:16 -0700

    just a little wordsmithing

--- scripts/index.js
+++ scripts/index.js
@@ -15,12 +15,12 @@ function setOccupancyDisplay(occupancy) {
     //console.log(`setOccupancyDisplay: ${occupancy}`);
     if (occupancy == 2) {
         document.getElementById('glyph').innerHTML = '\u278A';
-        document.getElementById('occupancy').innerHTML = '1 out of 3 spaces are available in this space.';
+        document.getElementById('occupancy').innerHTML = '1 out of 3 spots available in this space.';
         document.getElementById('instruction').innerHTML = 'Tap your fob to check in or out of this room.';
     } else {
         // Max Capacity
         document.getElementById('glyph').innerHTML = '\u2716';
-        document.getElementById('occupancy').innerHTML = 'Space is now at capacity: 3 spots out of 3 are taken.';
-        document.getElementById('instruction').innerHTML = 'Please wait until a spot open up.';
+        document.getElementById('occupancy').innerHTML = 'Space is now at capacity: 3 out of 3 spots taken.';
+        document.getElementById('instruction').innerHTML = 'Please wait until a spot opens up.';
     }
 }
