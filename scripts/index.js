90e802ee980b6b440e5724837f4d7ede05e495c9
Hal Rodriguez <halr66@gmail.com>
2020-06-30 15:13:44 -0700

    Added handling of keypresses.

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,48 @@
 /* index.js */
 
-async function reportAttempt(credential) {
-    //console.log(`reportAttempt: ${credential} (currently not implemented!)`);
-    //TODO: use the Fetch API
-    content = document.getElementById('glyph').innerHTML
-    if (content == '\u278A') {
-        setOccupancyDisplay(3);
+var glyphDiv;
+var occupancyDiv;
+var instructionDiv;
+var tmpCredential = '';
+
+function pagedLoaded() {
+    console.log(`pagedLoaded...`);
+    glyphDiv =  document.getElementById('glyph');
+    occupancyDiv = document.getElementById('occupancy');
+    instructionDiv = document.getElementById('instruction');
+
+    //TODO: Set up display based on stored or fetched occupancy
+    setOccupancyDisplay(2);
+    // set up listening for events
+    document.onkeypress = handleFobTag;
+}
+
+function handleFobTag(e) {
+    var evt = e || window.event;
+    //console.log('handleFobTag: ' + evt.type);
+    console.log('handleFobTag: key is ' + evt.key);
+    //console.log('handleFobTag: keyCode is ' + evt.keyCode);
+    //console.log('handleFobTag: charCode is ' + evt.charCode);
+    if (evt.keyCode == 13) {
+        reportAttempt(tmpCredential);
+        tmpCredential = '';
     } else {
-        setOccupancyDisplay(2);
+        tmpCredential += evt.key;
     }
 }
 
+//TODO: triggering reportAttempt on CR/Enter/13
+//TODO: capture local setup data
+
+async function reportAttempt(credential) {
+    console.log(`reportAttempt: ${credential}`);
+    //TODO: use the Fetch API
+    // refresh the page
+    //location = location;
+}
+
 function setOccupancyDisplay(occupancy) {
-    //console.log(`setOccupancyDisplay: ${occupancy}`);
+    console.log(`setOccupancyDisplay: ${occupancy}`);
     if (occupancy == 2) {
         document.getElementById('glyph').innerHTML = '\u278A';
         document.getElementById('occupancy').innerHTML = '1 out of 3 spots available in this space.';
